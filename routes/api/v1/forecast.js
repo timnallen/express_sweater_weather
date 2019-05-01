c4ce6db63bbb44348d6a96e3b2b90147f4804dee
Tim Allen <tnelsallen3@gmail.com>
2019-05-01 15:33:11 -0600

    Added env variables

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -2,16 +2,17 @@ var express = require("express");
 var router = express.Router();
 const fetch = require('node-fetch');
 var pry = require("pryjs");
+require("dotenv").config();
 
 router.get("/", function(req, res, next) {
-  var geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=AIzaSyA_bCoCAcfgE6KyxBiUVSalNCa4OSD9oXs`
+  var geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GEOCODING_KEY}`
   var locationName;
   fetch(geocodingUrl)
     .then(response => response.json())
     .then(result => {
       let location = result.results[0].geometry.location
       locationName = result.results[0].formatted_address
-      let darkSkyUrl = `https://api.darksky.net/forecast/e45f53b12ad26c9fa1dbf57bf6be99cb/${location.lat},${location.lng}`
+      let darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${location.lat},${location.lng}`
       return fetch(darkSkyUrl)
     })
     .then(response => response.json())
