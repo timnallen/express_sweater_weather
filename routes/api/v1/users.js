7af4b042fd447023da259f3ba0d52460fdb36ce9
Tim Allen <tnelsallen3@gmail.com>
2019-04-30 20:51:23 -0600

    Adds conditional for password matching confirmation

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,21 +3,26 @@ var router = express.Router();
 var User = require('../../../models').User;
 
 router.post("/", function(req, res, next) {
-  User.create(
-    {
-      email: req.body.email,
-      password_digest: req.body.password_digest,
-      api_key: api_key
-    }
-  )
-    .then(user => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify(api_key))
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(500).send({ error });
-    });
+  if (req.body.password === req.body.password_confirmation) {
+    User.create(
+      {
+        email: req.body.email,
+        password_digest: req.body.password_digest,
+        api_key: api_key
+      }
+    )
+      .then(user => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify(api_key))
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error });
+      });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send("Passwords do not match.")
+  }
 });
 
 module.exports = router;
