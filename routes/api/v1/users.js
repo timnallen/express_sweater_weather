cb56854db49bd5862e8f16f15a07747cf44ca994
Tim Allen <tnelsallen3@gmail.com>
2019-04-30 20:47:44 -0600

    Adds post route with user creation

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,4 +1,23 @@
 var express = require("express");
 var router = express.Router();
+var User = require('../../../models').User;
+
+router.post("/", function(req, res, next) {
+  User.create(
+    {
+      email: req.body.email,
+      password_digest: req.body.password_digest,
+      api_key: api_key
+    }
+  )
+    .then(user => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(api_key))
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
 
 module.exports = router;
