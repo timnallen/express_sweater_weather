fa5e61a5f4854a58da88120aef77157a733bd275
Tim Allen <tnelsallen3@gmail.com>
2019-04-30 21:30:06 -0600

    set routes correctly in app.js

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
 var bcrypt = require("bcrypt");
+var crypto = require("crypto");
 const saltRounds = 10;
 var User = require('../../../models').User;
 
 router.post("/", function(req, res, next) {
   if (req.body.password === req.body.password_confirmation) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash)) {
+    var api_key = api_key();
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
       User.create(
         {
           email: req.body.email,
@@ -16,17 +18,25 @@ router.post("/", function(req, res, next) {
       )
         .then(user => {
           res.setHeader("Content-Type", "application/json");
-          res.status(201).send(JSON.stringify(api_key))
+          res.status(201).send(JSON.stringify({api_key}))
         })
         .catch(error => {
           res.setHeader("Content-Type", "application/json");
           res.status(500).send({ error });
         });
-      };
+      });
   } else {
     res.setHeader("Content-Type", "application/json");
-    res.status(401).send("Passwords do not match.")
+    res.status(401).send({"Passwords do not match."})
   }
 });
+
+function api_key() {
+  // do {
+    var api_key = crypto.randomBytes(20).toString('hex');
+  // }
+  // while (User.findOne({ where: {api_key: api_key} }) !== null);
+  return api_key
+};
 
 module.exports = router;
