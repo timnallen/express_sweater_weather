07d6859f1c4f543ffc9e01734092e8f842dd3c7e
Tim Allen <tnelsallen3@gmail.com>
2019-04-30 21:00:02 -0600

    Adds bcrypt and encryptes password to be stored in db

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,24 +1,28 @@
 var express = require("express");
 var router = express.Router();
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
 var User = require('../../../models').User;
 
 router.post("/", function(req, res, next) {
   if (req.body.password === req.body.password_confirmation) {
-    User.create(
-      {
-        email: req.body.email,
-        password_digest: req.body.password_digest,
-        api_key: api_key
-      }
-    )
-      .then(user => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(201).send(JSON.stringify(api_key))
-      })
-      .catch(error => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(500).send({ error });
-      });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash)) {
+      User.create(
+        {
+          email: req.body.email,
+          password_digest: hash,
+          api_key: api_key
+        }
+      )
+        .then(user => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(201).send(JSON.stringify(api_key))
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send({ error });
+        });
+      };
   } else {
     res.setHeader("Content-Type", "application/json");
     res.status(401).send("Passwords do not match.")
