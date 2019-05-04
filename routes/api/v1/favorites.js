9fda7607773cf14e3d96910f2431c3d45d9d4b7b
Tim Allen <tnelsallen3@gmail.com>
2019-05-04 10:48:00 -0600

    finds or creates location based on if it exists in database

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,19 +1,60 @@
 var express = require("express");
 var router = express.Router();
+const fetch = require('node-fetch');
 var pry = require("pryjs");
+require("dotenv").config();
 var Location = require('../../../models').Location;
+var User = require('../../../models').User;
 
 router.post("/", function(req, res, next) {
-  Location.findAll({
-    where: {
-      location: req.body.location
-    }
-  })
-    .then(location => {
-      if (location.length === 0) {
-        
+  if (req.body.api_key) {
+    User.findAll(
+      {
+        where: {
+          api_key: req.body.api_key
+        }
       }
-    })
+    )
+      .then(user => {
+        if (user.length === 0) {
+          res.setHeader("Content-Type", "application/json");
+          res.status(401).send("Unauthorized access")
+        } else {
+          Location.findAll(
+            {
+              where: {
+                location: req.body.location
+              }
+            }
+          )
+            .then(location => {
+              var favorite;
+              if (location.length === 0) {
+                var geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.location}&key=${process.env.GEOCODING_KEY}`
+                fetch(geocodingUrl)
+                  .then(response => response.json())
+                  .then(result => {
+                    Location.create(
+                      {
+                        location: result.results[0].formatted_address,
+                        lat: result.results[0].geometry.location.lat,
+                        lng: result.results[0].geometry.location.lng
+                      }
+                    )
+                      .then(new_location => {
+                        favorite = new_location.dataValues;
+                      })
+                  })
+              } else {
+                favorite = location[0].dataValues;
+              }
+            })
+        }
+      })
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send("An authentic API key must be provided")
+  }
 })
 
 module.exports = router;
